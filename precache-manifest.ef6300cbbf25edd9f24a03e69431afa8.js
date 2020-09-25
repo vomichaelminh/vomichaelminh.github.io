03bfa8640c6f0d45c25b6d44bc4e9eb245453338
vomichaelminh <vomm@uci.edu>
2020-09-25 15:10:39 -0700

    Updates

---
+++ precache-manifest.ef6300cbbf25edd9f24a03e69431afa8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b470ce83392816d13d1b609898602c35",
+    "url": "/vomichaelminh.github.io/index.html"
+  },
+  {
+    "revision": "92e575ab685199dddfd3",
+    "url": "/vomichaelminh.github.io/static/css/main.d5e41ab9.chunk.css"
+  },
+  {
+    "revision": "7c7b469e6aa7a14c3a76",
+    "url": "/vomichaelminh.github.io/static/js/2.9ff190df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vomichaelminh.github.io/static/js/2.9ff190df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e575ab685199dddfd3",
+    "url": "/vomichaelminh.github.io/static/js/main.d6b19c0c.chunk.js"
+  },
+  {
+    "revision": "b43c1c51f93c0a509061",
+    "url": "/vomichaelminh.github.io/static/js/runtime-main.f4b50337.js"
+  },
+  {
+    "revision": "28c4902e24cfd8c569a502ed1a78d69f",
+    "url": "/vomichaelminh.github.io/static/media/bluejestic.28c4902e.png"
+  },
+  {
+    "revision": "94405fe3cf295381d7ab4528cda591bd",
+    "url": "/vomichaelminh.github.io/static/media/email-icon.94405fe3.png"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/vomichaelminh.github.io/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "369d2a847d6d7aa6ef24fc4b1a17204f",
+    "url": "/vomichaelminh.github.io/static/media/img-home.369d2a84.jpg"
+  },
+  {
+    "revision": "d66c625dce1e7a2dbdb121990110ae44",
+    "url": "/vomichaelminh.github.io/static/media/letsEat.d66c625d.jpg"
+  },
+  {
+    "revision": "a08f59f44e32ab5f5fe579b7b4088b97",
+    "url": "/vomichaelminh.github.io/static/media/linkedin.a08f59f4.png"
+  },
+  {
+    "revision": "1146cf88ffe39a401275a120d73f1058",
+    "url": "/vomichaelminh.github.io/static/media/profile.1146cf88.jpg"
+  },
+  {
+    "revision": "1053c0bf82b15ad9a9c1347401ce4684",
+    "url": "/vomichaelminh.github.io/static/media/studyplanner.1053c0bf.jpg"
+  },
+  {
+    "revision": "44940ebaf990babfbb8afcf49ee116f1",
+    "url": "/vomichaelminh.github.io/static/media/uci_ics.44940eba.jpg"
+  },
+  {
+    "revision": "2f597fb5f03bc1a94d22b3405ee78b0f",
+    "url": "/vomichaelminh.github.io/static/media/website.2f597fb5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal_website/precache-manifest.c17e298adfff836779bc46ec8cc1ea53.js"
+  "/vomichaelminh.github.io/precache-manifest.ef6300cbbf25edd9f24a03e69431afa8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal_website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vomichaelminh.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
